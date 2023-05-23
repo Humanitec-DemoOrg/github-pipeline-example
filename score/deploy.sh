44e26578140ef7f78e431475fdfc5808d5863eb7
nickhumanitec <121984975+nickhumanitec@users.noreply.github.com>
2023-05-23 16:32:58 -0600

    Update deploy.sh

--- score/deploy.sh
+++ score/deploy.sh
@@ -2,23 +2,23 @@
 set -e
 
 export DELTA_ID=`score-humanitec delta --api-url $HUMANITEC_URL --token $HUMANITEC_TOKEN --org $HUMANITEC_ORG --app $HUMANITEC_APP --env $HUMANITEC_ENVIRONMENT -f score.debug.yaml --extensions extensions.debug.yaml --overrides overrides.debug.yaml | jq .id -r`
+echo $DELTA_ID
 
-curl -s \
+CURRENT_SET_ID="$(curl -s \
   "https://api.humanitec.io/orgs/${HUMANITEC_ORG}/apps/${HUMANITEC_APP}/envs/${HUMANITEC_ENVIRONMENT}" \
-  -H "Authorization: Bearer ${HUMANITEC_TOKEN}" 
+  -H "Authorization: Bearer ${HUMANITEC_TOKEN}" | jq -r .last_deploy.set_id)"
 
-
-# CURRENT_SET_ID="$(curl -s \
-#   "https://api.humanitec.io/orgs/${HUMANITEC_ORG}/apps/${HUMANITEC_APP}/envs/${HUMANITEC_ENVIRONMENT}" \
-#   -H "Authorization: Bearer ${HUMANITEC_TOKEN}" | jq -r .last_deploy.set_id)"
+echo $CURRENT_SET_ID
 
 # # Apply the delta from the Score file to that deployment set returning the new set ID
-# NEW_SET_ID="$(curl -s \
-#   -X POST \
-#   "https://api.humanitec.io/orgs/${HUMANITEC_ORG}/apps/${HUMANITEC_APP}/sets/${CURRENT_SET_ID}" \
-#   -H "Authorization: Bearer ${HUMANITEC_TOKEN}" \
-#   -H "Content-Type: application/json" \
-#   -d "${DELTA_ID}" | jq -r .)"
+NEW_SET_ID="$(curl -s \
+  -X POST \
+  "https://api.humanitec.io/orgs/${HUMANITEC_ORG}/apps/${HUMANITEC_APP}/sets/${CURRENT_SET_ID}" \
+  -H "Authorization: Bearer ${HUMANITEC_TOKEN}" \
+  -H "Content-Type: application/json" \
+  -d "${DELTA_ID}" | jq -r .)"
+
+echo $NEW_SET_ID
 
 # # Diff the new to the current deployment set and output the result
 # curl -s \
