26dcea34e8f9d6d85e306782d06f097d718f861c
nickhumanitec <121984975+nickhumanitec@users.noreply.github.com>
2023-05-23 16:31:03 -0600

    Update deploy.sh

--- score/deploy.sh
+++ score/deploy.sh
@@ -3,20 +3,25 @@ set -e
 
 export DELTA_ID=`score-humanitec delta --api-url $HUMANITEC_URL --token $HUMANITEC_TOKEN --org $HUMANITEC_ORG --app $HUMANITEC_APP --env $HUMANITEC_ENVIRONMENT -f score.debug.yaml --extensions extensions.debug.yaml --overrides overrides.debug.yaml | jq .id -r`
 
-CURRENT_SET_ID="$(curl -s \
-  "https://api.humanitec.io/orgs/${HUMANITEC_ORG}/apps/${HUMANITEC_APP}/envs/${HUMANITEC_ENVIRONMENT}" \
-  -H "Authorization: Bearer ${HUMANITEC_TOKEN}" | jq -r .last_deploy.set_id)"
-
-# Apply the delta from the Score file to that deployment set returning the new set ID
-NEW_SET_ID="$(curl -s \
-  -X POST \
-  "https://api.humanitec.io/orgs/${HUMANITEC_ORG}/apps/${HUMANITEC_APP}/sets/${CURRENT_SET_ID}" \
-  -H "Authorization: Bearer ${HUMANITEC_TOKEN}" \
-  -H "Content-Type: application/json" \
-  -d "${DELTA_ID}" | jq -r .)"
-
-# Diff the new to the current deployment set and output the result
 curl -s \
-  "https://api.humanitec.io/orgs/${HUMANITEC_ORG}/apps/${HUMANITEC_APP}/sets/${NEW_SET_ID}/diff/${CURRENT_SET_ID}" \
-  -H "Authorization: Bearer ${HUMANITEC_TOKEN}" \
-  -H "Content-Type: application/json" | jq > /tmp/diff.txt
+  "https://api.humanitec.io/orgs/${HUMANITEC_ORG}/apps/${HUMANITEC_APP}/envs/${HUMANITEC_ENVIRONMENT}" \
+  -H "Authorization: Bearer ${HUMANITEC_TOKEN}" 
+
+
+# CURRENT_SET_ID="$(curl -s \
+#   "https://api.humanitec.io/orgs/${HUMANITEC_ORG}/apps/${HUMANITEC_APP}/envs/${HUMANITEC_ENVIRONMENT}" \
+#   -H "Authorization: Bearer ${HUMANITEC_TOKEN}" | jq -r .last_deploy.set_id)"
+
+# # Apply the delta from the Score file to that deployment set returning the new set ID
+# NEW_SET_ID="$(curl -s \
+#   -X POST \
+#   "https://api.humanitec.io/orgs/${HUMANITEC_ORG}/apps/${HUMANITEC_APP}/sets/${CURRENT_SET_ID}" \
+#   -H "Authorization: Bearer ${HUMANITEC_TOKEN}" \
+#   -H "Content-Type: application/json" \
+#   -d "${DELTA_ID}" | jq -r .)"
+
+# # Diff the new to the current deployment set and output the result
+# curl -s \
+#   "https://api.humanitec.io/orgs/${HUMANITEC_ORG}/apps/${HUMANITEC_APP}/sets/${NEW_SET_ID}/diff/${CURRENT_SET_ID}" \
+#   -H "Authorization: Bearer ${HUMANITEC_TOKEN}" \
+#   -H "Content-Type: application/json" | jq > /tmp/diff.txt
