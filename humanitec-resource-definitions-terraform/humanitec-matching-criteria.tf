d153d0f09721426918526b64f4d2b36623c51465
nickhumanitec <121984975+nickhumanitec@users.noreply.github.com>
2023-05-23 15:14:13 -0600

    Create humanitec-matching-criteria.tf

---
+++ humanitec-resource-definitions-terraform/humanitec-matching-criteria.tf
@@ -0,0 +1,19 @@
+resource "humanitec_resource_definition_criteria" "postgres_dev" {
+  resource_definition_id = humanitec_resource_definition.postgres_dev.id
+  app_id                 = humanitec_application.app.id
+}
+
+resource "humanitec_resource_definition_criteria" "mariadb_dev" {
+  resource_definition_id = humanitec_resource_definition.postgres_dev.id
+  app_id                 = humanitec_application.app.id
+}
+
+resource "humanitec_resource_definition_criteria" "postgres_prod" {
+  resource_definition_id = humanitec_resource_definition.postgres_prod.id
+  app_id                 = humanitec_application.app.id
+}
+
+resource "humanitec_resource_definition_criteria" "mariadb_prod" {
+  resource_definition_id = humanitec_resource_definition.postgres_prod.id
+  app_id                 = humanitec_application.app.id
+}
