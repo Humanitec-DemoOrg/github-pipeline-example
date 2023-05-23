7d56f3e26bd8f1c998a36bb422896eff8f9b7a56
nickhumanitec <121984975+nickhumanitec@users.noreply.github.com>
2023-05-23 16:53:52 -0600

    Update deploy.sh

--- score/deploy.sh
+++ score/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-export DELTA=`score-humanitec delta --api-url $HUMANITEC_URL --token $HUMANITEC_TOKEN --org $HUMANITEC_ORG --app $HUMANITEC_APP --env $HUMANITEC_ENVIRONMENT -f score.debug.yaml --extensions extensions.debug.yaml --overrides overrides.debug.yaml`
+export DELTA=`score-humanitec delta --api-url $HUMANITEC_URL --token $HUMANITEC_TOKEN --org $HUMANITEC_ORG --app $HUMANITEC_APP --env $HUMANITEC_ENVIRONMENT -f score.debug.yaml --extensions extensions.debug.yaml --overrides overrides.debug.yaml | jq 'del(.metadata.url)'`
 echo $DELTA
 
 CURRENT_SET_ID="$(curl -s \
@@ -12,14 +12,6 @@ echo $CURRENT_SET_ID
 
 # # Apply the delta from the Score file to that deployment set returning the new set ID
 
-curl -s \
-  -X POST \
-  "https://api.humanitec.io/orgs/${HUMANITEC_ORG}/apps/${HUMANITEC_APP}/sets/${CURRENT_SET_ID}" \
-  -H "Authorization: Bearer ${HUMANITEC_TOKEN}" \
-  -H "Content-Type: application/json" \
-  -d "${DELTA}"
-
-  
 NEW_SET_ID="$(curl -s \
   -X POST \
   "https://api.humanitec.io/orgs/${HUMANITEC_ORG}/apps/${HUMANITEC_APP}/sets/${CURRENT_SET_ID}" \
