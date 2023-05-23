da592e2609946aa23e94df2186555dc6ae810bfc
nickhumanitec <121984975+nickhumanitec@users.noreply.github.com>
2023-05-23 17:22:48 -0600

    updates

--- score/deploy.sh
+++ score/deploy.sh
@@ -4,6 +4,7 @@ set -e
 export DELTA=`score-humanitec delta --api-url $HUMANITEC_URL --token $HUMANITEC_TOKEN --org $HUMANITEC_ORG --app $HUMANITEC_APP --env $HUMANITEC_ENVIRONMENT -f score.debug.yaml --extensions extensions.debug.yaml --overrides overrides.debug.yaml | jq 'del(.metadata.url)'`
 echo "NEW DELTA"
 echo $DELTA
+echo $DELTA| jq > /tmp/new_delta.txt
 
 echo "CURRENT SET"
 curl -s \
@@ -18,7 +19,7 @@ CURRENT_DELTA_ID="$(curl -s \
   "https://api.humanitec.io/orgs/${HUMANITEC_ORG}/apps/${HUMANITEC_APP}/envs/${HUMANITEC_ENVIRONMENT}" \
   -H "Authorization: Bearer ${HUMANITEC_TOKEN}" | jq -r .last_deploy.delta_id)"
 
-  
+
 echo "Current SET ID"
 echo $CURRENT_SET_ID
 echo "Current DELTA ID"
@@ -26,7 +27,7 @@ echo $CURRENT_DELTA_ID
 
 curl -s \
   "https://api.humanitec.io/orgs/${HUMANITEC_ORG}/apps/${HUMANITEC_APP}/deltas/${CURRENT_DELTA_ID}" \
-  -H "Authorization: Bearer ${HUMANITEC_TOKEN}"
+  -H "Authorization: Bearer ${HUMANITEC_TOKEN}" | jq > /tmp/delta.txt
 
 
 # # Apply the delta from the Score file to that deployment set returning the new set ID
